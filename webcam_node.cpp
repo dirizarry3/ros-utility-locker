558f07259c584b845bf7c973844e87a9a78493e2
riskitforbisket <dirizarry3@gmail.com>
2017-03-25 13:57:10 -0500

    added comments, removed junk

--- webcam_node.cpp
+++ webcam_node.cpp
@@ -1,6 +1,4 @@
 #include <ros/ros.h>
-//#include <std_msgs/String.h>
-//#include <sensor_msgs/Image.msg>
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
@@ -12,28 +10,35 @@ int main(int argc, char** argv)
 {
    //init ros from potential command line args and give it a name
    ros::init(argc, argv, "webcam_publisher");
-   cout << "here1" << endl;
+
    //get node handle so we can publish the camera data
    ros::NodeHandle nHandle;
    image_transport::ImageTransport it(nHandle);
    image_transport::Publisher pub = it.advertise("camera_image", 1);
-   cout << "here2" << endl;
+   
+   //define the video source and open up the cv_image stream
+   //Note: for simplicity, I could just plug in a '0' which would infer
+   //the laptops onboard cam, but this allows for peripheral cams as well
    std::istringstream video_sourceCmd(argv[1]);
    int video_source;
-   
-   if(!(video_sourceCmd >> video_source)) return 1;
-   cout << "here3" << endl;
+   if(!(video_sourceCmd >> video_source)) {
+      return 1;
+   }
    cv::VideoCapture cap(video_source);
-   if(!cap.isOpened()) return 1;
+   if(!cap.isOpened()) {
+      return 1;
+   }
    
+   //declare and set variables before the ros spin
    cv::Mat frame;
    sensor_msgs::ImagePtr msg;
-   cout << "here4" << endl;
    ros::Rate loop_rate(5);
-   cout << "here5" << endl;
+
+   
    while(nHandle.ok()) {
       cap >> frame;
       if(!frame.empty()) {
+         //convert frame to ros image message and publish
          msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
          pub.publish(msg);
          cv::waitKey(1);
